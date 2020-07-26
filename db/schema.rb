20a99c74e54b84f9213377d19e39c58eac4733d7
Seidai Inoue <shengdajingshang@gmail.com>
2020-07-26 21:07:08 +0900

    モデル・テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_07_26_120324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "shops", force: :cascade do |t|
+    t.string "name"
+    t.string "number"
+    t.string "address"
+    t.string "access"
+    t.string "time"
+    t.string "rest"
+    t.string "takeout"
+    t.string "map"
+    t.string "introduce"
+    t.string "shop_image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.string "genre"
+    t.text "content"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "shop_id"
+    t.index ["shop_id"], name: "index_topics_on_shop_id"
+  end
+
+  add_foreign_key "topics", "shops"
 end
