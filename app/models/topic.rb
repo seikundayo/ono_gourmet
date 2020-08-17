4e35a8ce850ad40efbd93059c093081b859910b0
Seidai Inoue <shengdajingshang@gmail.com>
2020-08-17 17:58:32 +0900

    トピック詳細ページの右にサイドバーを追加

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,5 @@ class Topic < ApplicationRecord
   belongs_to :shop
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
-  is_impressionable
+  is_impressionable counter_cache: true
 end
