96d42d9c12575caa33bc563f72cada145ef898e4
Seidai Inoue <shengdajingshang@gmail.com>
2020-09-06 10:16:00 +0900

    サイトの微調整

--- app/controllers/like_lists_controller.rb
+++ app/controllers/like_lists_controller.rb
@@ -1,10 +1,13 @@
 class LikeListsController < ApplicationController
   def index
-    if user_signed_in?
       @like_lists = current_user.likes
-    else
-      flash[:alert] = "「行ってみたいリスト」を表示するにはログインしてください"
-      redirect_to topics_path
-    end
   end
 end
+
+
+# if user_signed_in?
+#    @like_lists = current_user.likes
+#  else
+#    flash[:alert] = "「行ってみたいリスト」を表示するにはログインしてください"
+#    redirect_to topics_path
+#  end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,7 @@
 class LikesController < ApplicationController
   before_action :set_post
   def create
-    if user_signed_in?
       current_user.likes.create!(topic_id: params[:topic_id])
-    else
-      flash[:alert] = "「いいね」をする場合はログインしてください"
-      redirect_to topics_path
-    end
   end
 
   def destroy
@@ -19,3 +14,11 @@ class LikesController < ApplicationController
     @like = Topic.find(params[:topic_id])
   end
 end
+
+
+# if user_signed_in?
+#   current_user.likes.create!(topic_id: params[:topic_id])
+# else
+#   flash[:alert] = "「いいね」をする場合はログインしてください"
+#   redirect_to topics_path
+# end
