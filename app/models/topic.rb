86076c886bdd91d26a37397a5ea10ff48e4998d0
Seidai Inoue <shengdajingshang@gmail.com>
2020-10-01 19:52:53 +0900

    バリデーション修正

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,12 +1,12 @@
 class Topic < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_rich_text :content
-  belongs_to :shop
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   is_impressionable counter_cache: true
   validates :title, presence: true
   validates :genre, presence: true
   validates :image, presence: true
-  validates :shop_id, presence: true
+  validates :content, presence: true
+  belongs_to :shop
 end
